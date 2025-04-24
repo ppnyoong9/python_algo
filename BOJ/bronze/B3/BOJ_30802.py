b0f0060da3e3c56a52740e4cf439d8939d2df276
김진효 <ppnyonng@gmail.com>
2025-04-24 18:00:21 +0900

    .

---
+++ BOJ/bronze/B3/BOJ_30802.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+input = sys.stdin.readline
+
+
+
+'''
+BOJ 30802번 웰컴 키트
+'''
+
+# 입력: 참가자의 수(N), 티셔츠 사이즈 별 신청자의 수(S, M, L, XL, XXL, XXXL), 티셔츠 묶음 수(T), 펜의 묶음 수(P)
+# 출력: 주문해야 하는 티셔츠 최소 묶음 수, 펜 묶음 수
