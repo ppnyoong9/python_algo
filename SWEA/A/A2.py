45a724150b8b46904348ebe5b368cc2b4ed0e735
ppnyoong <ppnyoong@gmail.com>
2025-08-26 17:52:45 +0900

    [250826] SWEA

--- SWEA/A/A2.py
+++ SWEA/A/A2.py
@@ -1,7 +1,25 @@
 import sys
 sys.stdin = open('input.txt','r')
 
+# 입력: 테스트케이스(T), 지하철 역(N), 역 별 이용 수 배열
+# 출력: 제일 높은 타당도
 
-# 입력:
-# 출력:
 
+t = int(input())
+for tc in range(1, t+1):
+    # 역 수, 지하철 이용객 수 배열 받기
+    n = int(input())
+    arr = list(map(int,input().split()))
+
+    ans = 0
+    
+    # 역은 무조건 한 칸 씩 띄워져 있어야한다
+    for i in range(n-6):
+        for j in range(i+2, n-4):
+            for p in range(j+2, n-2):
+                for q in range(p+2, n):
+                    # 겹치지 않는 경우 2가지가 있다!
+                    ans = max(ans,(arr[i]+arr[j])**2 + (arr[p]+arr[q])**2)
+                    ans = max(ans, (arr[i]+arr[q])**2 + (arr[j]+arr[p])**2)
+   
+    print(f'#{tc} {ans}')
---
+++ SWEA/A/A4.py
@@ -0,0 +1,27 @@
+import sys
+sys.stdin = open('input.txt','r')
+
+# 입력: 테스트케이스(T), 지도 한 변의 길이(N), 산악 배열
+# 출력: 조난자의 위치까지 최소 연료를 소비하는 경로에 해당하는 연료 값
+
+
+def bfs(s):
+    # 시작점을 넣고 시작
+    q = [s]
+
+    while q:
+        c = q.pop()
+
+
+
+for tc in range(1,t+1):
+
+    n = int(input())
+    arr = [list(map(int,input().split())) for _ in range(n)]
+
+    ans = 0
+
+    visited = [0] * (n+1)
+
+
+
